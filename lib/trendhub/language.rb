0629fe8cf2aebe3c11f6c3433a79684677da41c3
Son Dang <sondangdev@gmail.com>
2019-02-23 22:12:31 +0700

    Add language update method

--- lib/trendhub/language.rb
+++ lib/trendhub/language.rb
@@ -11,5 +11,17 @@ module Trendhub
     def all
       File.read(language_path).split("\n")
     end
+
+    def update
+      require 'http'
+      require 'yaml'
+
+      language_url = "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
+      response = HTTP.get(language_url)
+      languages = YAML.load(response.to_s).keys.map do |language|
+        language.downcase.gsub(" ", "-")
+      end
+      File.write(language_path, languages.join("\n"))
+    end
   end
 end
